1b16a959951f54197e8bc96cc2ab71bc25ad889b
Ross Johnson <Rossjohnsonpro@outlook.com>
2020-06-09 22:18:55 +0000

    Add changes to comments in pull request

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -20,16 +20,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.google.gson.*;
+import com.google.gson.Gson;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 
-/** Servlet that handles comments */
+/** Servlet that handles comments through post and TODO: get requests */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-    ArrayList<String> Greetings = new ArrayList();
-    ArrayList<String> commentRepo = new ArrayList<String>();
+    public static final String JSON_CONTENT_TYPE = "application/json";
+    public static final String COMMENT_PROPERTY = "comment";
+    public static final String TIMESTAMP_PROPERTY = "timestamp";
+    public static final String COMMENT_ENTITY = "comment";
+    private final ArrayList<String> Greetings = new ArrayList();
+    private final ArrayList<String> commentRepo = new ArrayList<String>();
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -37,13 +41,19 @@ public class DataServlet extends HttpServlet {
   }
   @Override
     public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException{
-        Entity commentEntity = new Entity("comment");
-        String jsonstring = req.getParameter("comment");
-        commentEntity.setProperty("comment",jsonstring);
-        long timestamp = System.currentTimeMillis();
-        commentEntity.setProperty("timestamp", timestamp);
+        Entity commentEntity = new Entity(COMMENT_ENTITY);
+
+        String jsonString = req.getParameter("comment");
+        commentEntity.setProperty(COMMENT_PROPERTY, jsonstring);
+
+        long currentTimeMillis = System.currentTimeMillis();
+        commentEntity.setProperty(TIMESTAMP_PROPERTY, currentTimeMillis);
+
         datastore.put(commentEntity);
         commentRepo.add(jsonstring);
         res.sendRedirect("/index.html");
+
+        //TODO: Implement a backend that posts and 
+        //fetches comments for the user
     }
 }
